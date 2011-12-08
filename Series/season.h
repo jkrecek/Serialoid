38c0d283b9d5f20d884246f44ea55ba2bf53794d
KillerFrca <killerfrca@gmail.com>
2011-12-08 17:54:40 +0100

    GMT values in Season are unused now
    Signed-off-by: KillerFrca <killerfrca@gmail.com>

--- Series/season.h
+++ Series/season.h
@@ -13,18 +13,14 @@ class Season
         Season(uint seasonNumber) : seasonNumber_m(seasonNumber) {}
 
         void SetInfo(QString info) { Info_m = info; }
-        void SetHourdiff(int diff) { airHourDiff_m = diff; }
-
         uint GetNumber() const { return seasonNumber_m; }
-        int GetHourDiff() const { return airHourDiff_m; }
-        int GetSecondsDiff() const { return airHourDiff_m*3600; }
+
         QString GetInfo() const { return Info_m; }
         EpisodeMap& GetEpisodes() { return lEpisodes_m; }
         Episode* GetEpisode(uint Id) const { return lEpisodes_m.value(Id); }
     private:
         uint seasonNumber_m;
         QString Info_m;
-        int airHourDiff_m;
 
         EpisodeMap lEpisodes_m;
 };
